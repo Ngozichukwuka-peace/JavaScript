a2071e2897a6d9d062937f170873ccbb5049d95d
Ngozichukwuka-peace <ngozichukwukapeace@gmail.com>
2026-08-19 10:08:11 +0100

    This is my first commit

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
     
-let Role = "Admin";
-let passWord = "TheMaster";
+// let Role = "Admin";
+// let passWord = "TheMaster";
 
-if (Role === "Admin") {
-    console.log('Enter Password');
-    if (passWord === "TheMaster") {
-        console.log("Welcome");
-    } else if (passWord != "TheMaster") {
-        console.log("Wrong Password");
-    }
-} else if (Role != "Admin") {
-    console.log("I dont know you");
-}
-
-
+// if (Role === "Admin") {
+//     console.log('Enter Password');
+//     if (passWord === "TheMaster") {
+//         console.log("Welcome");
+//     } else if (passWord != "TheMaster") {
+//         console.log("Wrong Password");
+//     }
+// } else if (Role != "Admin") {
+//     console.log("I dont know you");
+// }
 
 
 
+
+// let arrString = [1,2,3,4,5,6,7,8,9,10];
+// let arr =[];
+
+// let i = 0;
+
+// while (i < arrString.length) {
+//     if (arrString[i] % 2 === 0){
+//         arr.push(arrString[i]);
+//     }
+//     i++;
+// }
+// console.log(arr);
+
+// for (let i = 0; i <= 3; i++) {
+//     for (let j = 0; j <= 3; j++) {
+//     console.log(i,j);
+//     }
+//     // console.log(i);
+// }
+
+
+
+
+// function countDown(number) {
+//     if (number <=0) {
+//         console.log("Blast off!");
+//         return;
+//     }    
+//     console.log(number);
+//     countDown(number - 1);
+// }
+// countDown(3);
+ 
+
+ let heading = document.getElementById('displays');
+ heading.innerText = "This will not be my last JavaScrit code"
+
+// heading.textContent = "Hello";
+
+console.log(heading);
